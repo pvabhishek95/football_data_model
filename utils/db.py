e38868ba328a5c1fab9e3cf76b08f9b12a5beba6
Abhishek Pv <abhishek.pv@ITs-MacBook-Pro.local>
2023-08-31 10:40:17 -0400

    Added env and modified conn string

--- utils/db.py
+++ utils/db.py
@@ -6,17 +6,26 @@ from sqlalchemy import create_engine
 
 
 def load_df_to_postgress(df_list: [pd.DataFrame], table_names: List[str]):
-    # establish connections
-    conn_string = 'postgresql://postgres:admin@127.0.0.1/soccer_data_model'
 
-    db = create_engine(conn_string)
-    conn = db.connect()
-    conn1 = psycopg2.connect(
-        database=os.getenv("WAREHOUSE_DB"),
+    #get conn parameters
+    database=os.getenv("WAREHOUSE_DB"),
     user=os.getenv("WAREHOUSE_USER"), 
     password=os.getenv("WAREHOUSE_PASSWORD"), 
     host=os.getenv("WAREHOUSE_HOST"), 
     port= os.getenv("WAREHOUSE_PORT")
+
+
+    # establish connections
+    conn_string = f'postgresql://{user}:{password}@{host}:{port}/{database}'
+
+    db = create_engine(conn_string)
+    conn = db.connect()
+    conn1 = psycopg2.connect(
+        database=database,
+    user=user, 
+    password=password, 
+    host=host, 
+    port=port
     )
 
     conn1.autocommit = True
